c00cfe1d313a103bb61f6e550e63fb3176965844
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2023-12-29 12:10:45 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+       function showAlert() {
+            alert("Welcome to Rimzath Ahamed's Profile!");
+        }
+
+       function processForm() {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+
+            alert("Your Name: " + name + "\nAnd your Email: " + email + name+ "\nyou can see my details in this page ");
+        }
